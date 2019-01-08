9d9f559c33d4436d9abdcbb1070870ad2f83c6e0
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-08 12:37:32 -0600

    Implement changeColor stepper action

--- ColorSwitcher/ViewController.swift
+++ ColorSwitcher/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Variables
+    
+    let colorController = ColorController()
 
+    @IBOutlet weak var label: UILabel!
+    
+    
+    // MARK: - Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.text = nil
     }
 
-
+    @IBAction func changeColor(_ sender: UIStepper) {
+        
+        // Modulo operator won't let value is goint to higher than colors.count
+        
+        let currentStepperValue = sender.value.magnitude // Absolute value, all positive value
+        colorController.currentColorIndex = Int(currentStepperValue) % colorController.colors.count
+        
+        if colorController.colorOn {
+            view.backgroundColor = colorController.currentColor.color
+            label.text = colorController.currentColor.name
+        } else {
+            view.backgroundColor = .white
+            label.text = nil
+        }
+        
+    }
+    
+    @IBAction func toggleColorOn(_ sender: UISwitch) {
+        
+    }
+    
 }
 
